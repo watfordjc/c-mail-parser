1d6826e88b0b8b6bddb50707e716f38aeb12a836
John Cook <coder-a599-7927@johncook.co.uk>
2023-07-04 12:15:54 +0100

    Map file to memory and print it to stdout

--- mail-parser.c
+++ mail-parser.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
 /*
 The following exit values are defined:
@@ -20,20 +25,48 @@ int main(int argc, char* argv[])
 	}
 
 	// Try to open the file
-	FILE *fp;
 	errno = 0;
-	fp = fopen(argv[1], "r");
-	if (fp == 0 && errno != 0) {
-		perror("I/O error");
+	int fd = open(argv[1], O_RDONLY, S_IRUSR);
+	if (fd == -1) {
+		perror("open() error: ");
 		exit(3);
 	}
 
-	// TODO: Do some work with file
+	// Get file size
+	struct stat sb;
+	errno = 0;
+	if (fstat(fd, &sb) == -1) {
+		perror("fstat() error: ");
+	}
+	printf("File size: %ld B\n", sb.st_size);
+
+	// Map file in memory
+	char *file_in_memory = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (file_in_memory == MAP_FAILED) {
+		perror("mmap() error: ");
+	}
+
+	// Separate above output from below output
+	printf("\n");
+
+	// TODO: Do some work on file
+
+	// Print the file
+	for (int i=0; i < sb.st_size; i++) {
+		printf("%c", file_in_memory[i]);
+	}
+	printf("\n");
+
+	// Unmap the file from memory
+	errno = 0;
+	if (munmap(file_in_memory, sb.st_size) == -1) {
+		perror("munmap() error: ");
+	}
 
 	// Try to close the file
 	errno = 0;
-	if (fclose(fp) != 0) {
-		perror("I/O error");
+	if (close(fd) == -1) {
+		perror("close() error: ");
 		exit(3);
 	}
 }
